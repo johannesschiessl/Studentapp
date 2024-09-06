5c59fc050490b34ab5d5a1f9defb0b74ece79b4b
Johannes Schießl <ignotumnomen@icloud.com>
2024-09-06 22:46:16 +0200

    feat(home): add home (#1)

---
+++ src/app/(app)/home/page.tsx
@@ -0,0 +1,9 @@
+import Greeting from "@/components/home/greeting";
+
+export default function HomePage() {
+  return (
+    <main className="mx-auto w-full max-w-5xl space-y-10">
+      <Greeting />
+    </main>
+  );
+}
