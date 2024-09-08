1468313dd8495b43457f6fc7af29766993f6f025
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-09-08 23:16:30 +0200

    feat(homework): add actions

---
+++ src/app/actions/homework.ts
@@ -0,0 +1,96 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { Task } from "@/types/homework";
+import { cookies } from "next/headers";
+export async function getHomeworkForCurrentSchoolYear(): Promise<Task[]> {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const currentSchoolYearId = getCurrentSchoolYearId();
+
+  const { data, error } = await supabase
+    .from("homework")
+    .select("*")
+    .eq("user_id", user.id)
+    .eq("school_year_id", currentSchoolYearId);
+
+  if (error) {
+    console.error("Error fetching homework:", error);
+    throw error;
+  }
+
+  return data as Task[];
+}
+
+export async function toggleTaskDone(id: number, currentDone: boolean) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("homework")
+    .update({ done: !currentDone })
+    .eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+
+  return data;
+}
+
+export async function addTask(task: Task) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("homework")
+    .insert({ ...task, school_year_id: getCurrentSchoolYearId() })
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+
+  return data;
+}
+
+export async function editTask(task: Task) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("homework")
+    .update({ ...task })
+    .eq("id", task.id)
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+
+  return data;
+}
+
+export async function deleteTask(id: number) {
+  const supabase = createClient();
+  const { data, error } = await supabase.from("homework").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+
+  return data;
+}
+
+// This function exists twice (one here and the other in ./school-year.ts) But having it here drastically speeds up the page load time, as there is no await.
+function getCurrentSchoolYearId(): number {
+  const cookieStore = cookies();
+  const currentSchoolYearId = cookieStore.get("currentSchoolYearId");
+  return currentSchoolYearId ? parseInt(currentSchoolYearId.value, 10) : 1;
+}
