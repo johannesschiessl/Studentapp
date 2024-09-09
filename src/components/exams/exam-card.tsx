430660ca3da28c61fa3a02b0bbc19094ed4049b5
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-09-09 19:23:25 +0200

    feat(exams): add card and list

---
+++ src/components/exams/exam-card.tsx
@@ -0,0 +1,80 @@
+import { Exam, ExamType } from "@/types/exams";
+import { Calendar, CalendarArrowDown } from "lucide-react";
+
+import { EditExamDialog } from "./edit-exam-dialog";
+import { useTranslation } from "@/hooks/use-translation";
+
+export default function ExamCard({
+  exam,
+  color,
+  onEdit,
+  onDelete,
+  examTypes,
+}: {
+  exam: Exam;
+  color: string;
+  onEdit: (exam: Exam) => void;
+  onDelete: (id: number) => void;
+  examTypes: ExamType[];
+}) {
+  const { t } = useTranslation();
+  return (
+    <EditExamDialog
+      exam={exam}
+      onEdit={(data: Exam) => onEdit(data)}
+      onDelete={(id: number) => onDelete(id)}
+      examTypes={examTypes}
+    >
+      <div
+        key={exam.id}
+        className="flex w-full cursor-pointer flex-col justify-between rounded-[1rem] border-2 p-4"
+      >
+        <div className="flex flex-col">
+          <div className="flex justify-between">
+            <div className="flex items-center space-x-2">
+              <h2 className="text-lg font-semibold">
+                {
+                  examTypes.find(
+                    (type) => parseInt(type.id) === exam.exam_type_id,
+                  )?.name
+                }
+              </h2>
+            </div>
+            {exam.grade !== undefined && (
+              <span className={`text-xl font-bold text-${color}-500`}>
+                {exam.grade}
+                {exam.grade_modifier !== "none" && (
+                  <span>{exam.grade_modifier}</span>
+                )}
+              </span>
+            )}
+          </div>
+          <p className="my-3 text-sm text-gray-600">
+            {exam.description ? exam.description : t("exams.no_description")}
+          </p>
+        </div>
+
+        <div className="flex-grow" />
+
+        <div className="mt-4 flex w-full flex-col space-y-2">
+          <span
+            className={`flex items-center text-sm font-medium text-muted-foreground`}
+          >
+            <Calendar className="mr-2 h-4 w-4" />
+            {t("exams.written_on")}{" "}
+            {new Date(exam.date_written).toLocaleDateString()}
+          </span>
+          {exam.date_returned && (
+            <span
+              className={`flex items-center text-sm font-medium text-muted-foreground`}
+            >
+              <CalendarArrowDown className="mr-2 h-4 w-4" />
+              {t("exams.returned_on")}{" "}
+              {new Date(exam.date_returned).toLocaleDateString()}
+            </span>
+          )}
+        </div>
+      </div>
+    </EditExamDialog>
+  );
+}
---
+++ src/components/exams/exam-list.tsx
@@ -0,0 +1,135 @@
+import { Exam, ExamType } from "@/types/exams";
+import ExamCard from "./exam-card";
+import { useEffect, useRef } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import { useTranslation } from "@/hooks/use-translation";
+
+export default function ExamList({
+  exams,
+  color,
+  onEdit,
+  onDelete,
+  examTypes,
+}: {
+  exams: Exam[];
+  color: string;
+  onEdit: (exam: Exam) => void;
+  onDelete: (id: number) => void;
+  examTypes: ExamType[];
+}) {
+  const upcomingExamListParent = useRef(null);
+  const pendingExamListParent = useRef(null);
+  const writtenExamListParent = useRef(null);
+  const examListParent = useRef(null);
+
+  useEffect(() => {
+    upcomingExamListParent.current &&
+      autoAnimate(upcomingExamListParent.current);
+    pendingExamListParent.current && autoAnimate(pendingExamListParent.current);
+    writtenExamListParent.current && autoAnimate(writtenExamListParent.current);
+    examListParent.current && autoAnimate(examListParent.current);
+  }, [
+    upcomingExamListParent,
+    pendingExamListParent,
+    writtenExamListParent,
+    examListParent,
+  ]);
+
+  const { t } = useTranslation();
+
+  const sortedExams = exams.sort(
+    (a, b) =>
+      new Date(b.date_written).getTime() - new Date(a.date_written).getTime(),
+  );
+
+  const today = new Date();
+
+  const upcomingExams = sortedExams.filter(
+    (exam) => new Date(exam.date_written) > today,
+  );
+  const pendingExams = sortedExams.filter(
+    (exam) => new Date(exam.date_written) <= today && !exam.grade,
+  );
+  const writtenExams = sortedExams.filter(
+    (exam) => new Date(exam.date_written) <= today && exam.grade,
+  );
+
+  return (
+    <div ref={examListParent} className="space-y-4">
+      {/* Upcoming Exams */}
+      {upcomingExams.length > 0 && (
+        <div>
+          <h2 className="mb-2 text-lg font-bold">{t("exams.upcoming")}</h2>
+          <div
+            ref={upcomingExamListParent}
+            className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0"
+          >
+            {upcomingExams.map((exam) => (
+              <ExamCard
+                examTypes={examTypes}
+                onEdit={(exam) => onEdit(exam)}
+                onDelete={(id) => onDelete(id)}
+                key={exam.id}
+                exam={exam}
+                color={color}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {/* Pending Exams */}
+      {pendingExams.length > 0 && (
+        <div>
+          <h2 className="mb-2 text-lg font-bold">{t("exams.pending")}</h2>
+          <div
+            ref={pendingExamListParent}
+            className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0"
+          >
+            {pendingExams.map((exam) => (
+              <ExamCard
+                examTypes={examTypes}
+                onEdit={(exam) => onEdit(exam)}
+                onDelete={(id) => onDelete(id)}
+                key={exam.id}
+                exam={exam}
+                color={color}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {/* Written Exams */}
+      {writtenExams.length > 0 && (
+        <div>
+          <h2 className="mb-2 text-lg font-bold">{t("exams.written")}</h2>
+          <div
+            ref={writtenExamListParent}
+            className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0"
+          >
+            {writtenExams.map((exam) => (
+              <ExamCard
+                examTypes={examTypes}
+                onEdit={(exam) => onEdit(exam)}
+                onDelete={(id) => onDelete(id)}
+                key={exam.id}
+                exam={exam}
+                color={color}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {/* No Exams */}
+      {upcomingExams.length === 0 &&
+        pendingExams.length === 0 &&
+        writtenExams.length === 0 && (
+          <div className="py-10">
+            <p className="text-center text-gray-500">{t("exams.no_exams")}</p>
+          </div>
+        )}
+    </div>
+  );
+}
