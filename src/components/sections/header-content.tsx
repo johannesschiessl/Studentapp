488c386fbf78c75379d89a699a4e7a5cd2ce6830
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2024-09-15 18:29:31 +0200

    feat(header): add settings-dialog and user menu

--- src/components/sections/header-content.tsx
+++ src/components/sections/header-content.tsx
@@ -2,7 +2,7 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { PanelLeftOpen } from "lucide-react";
+import { PanelLeftOpen, Settings } from "lucide-react";
 import { useSidebar } from "@/contexts/sidebar-context";
 import {
   Tooltip,
@@ -11,6 +11,8 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useTranslation } from "@/hooks/use-translation";
+import SettingsDialog from "../settings/settings-dialog";
+import UserMenu from "./user-menu";
 
 export function HeaderContent(user: any) {
   const { isOpen: isSidebarOpen, toggleSidebar } = useSidebar();
@@ -51,12 +53,32 @@ export function HeaderContent(user: any) {
       {/* Desktop */}
 
       <div className="hidden items-center gap-4 md:flex">
-        <Avatar>
-          <AvatarImage src={user?.user?.user_metadata.avatar_url} />
-          <AvatarFallback>
-            {user?.user?.user_metadata.name?.charAt(0)}
-          </AvatarFallback>
-        </Avatar>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <SettingsDialog>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="rounded-[1rem] text-neutral-500 hover:bg-neutral-200 dark:hover:bg-neutral-700"
+                >
+                  <Settings />
+                </Button>
+              </SettingsDialog>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{t("settings.open")}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <UserMenu>
+          <Avatar>
+            <AvatarImage src={user?.user?.user_metadata.avatar_url} />
+            <AvatarFallback>
+              {user?.user?.user_metadata.name?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+        </UserMenu>
       </div>
     </header>
   );
