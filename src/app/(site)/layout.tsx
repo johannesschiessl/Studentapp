65637fef24b81ce91df068049c098a1429d377bc
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2025-04-19 19:35:24 +0200

    chore: some more updates (#46)
    * chore: some more updates
    
    * fix: eslint error

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { AtSign, BookCopy, Instagram, Music2 } from "lucide-react";
+import { BookCopy } from "lucide-react";
 import Link from "next/link";
 
 export default function SiteLayout({
@@ -27,35 +27,6 @@ export default function SiteLayout({
       <main className="flex-1">{children}</main>
       <footer className="mb-10 py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24">
-          <div className="flex items-center space-x-4">
-            <Link
-              href="https://www.instagram.com/studentapp_"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-1 text-muted-foreground transition-colors hover:text-foreground"
-            >
-              <Instagram className="h-6 w-6" />
-              Instagram
-            </Link>
-            <Link
-              href="https://www.tiktok.com/@studentapp"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-1 text-muted-foreground transition-colors hover:text-foreground"
-            >
-              <Music2 className="h-6 w-6" />
-              TikTok
-            </Link>
-            <Link
-              href="https://www.threads.net/@studentapp_"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-1 text-muted-foreground transition-colors hover:text-foreground"
-            >
-              <AtSign className="h-6 w-6" />
-              Threads
-            </Link>
-          </div>
           <div className="flex items-center space-x-4">
             <Link
               href="/changelog"
